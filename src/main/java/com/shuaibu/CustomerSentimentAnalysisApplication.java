94b9af3d6cee86d6e99fdda9b18338eb1df2479e
shuaibu222 <shuaibuabdulkadir222@gmail.com>
2024-05-20 15:13:04 +0100

    initial project

---
+++ src/main/java/com/shuaibu/CustomerSentimentAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.shuaibu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSentimentAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerSentimentAnalysisApplication.class, args);
+	}
+
+}
